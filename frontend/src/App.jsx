1125996830424573337f0855501c39d7ff139cef
Yurii <yurii.rudenko26@gmail.com>
2025-01-30 21:51:37 +0200

    added-basic-page-mark

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import './styles/style.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="wrapper">
+
+        <div className='menu'>
+            1233
+        </div>
+
+        <div className='main'>
+            <div className="header">
+              header
+            </div>
+            <div className="body">
+              body
+            </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
