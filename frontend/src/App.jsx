42da84e84cc4acd77683183a79cb586eee027da5
Yurii <yurii.rudenko26@gmail.com>
2025-01-31 23:46:23 +0200

    added-header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import './styles/style.css';
 import { ReactComponent as Search } from './images/Search.svg';
+import { ReactComponent as Bell } from './images/Bell.svg';
+import { ReactComponent as Arrow } from './images/Arrow.svg';
 
 function App() {
   return (
@@ -16,6 +18,14 @@ function App() {
               <div className="input">
                 <Search className="search-icon"/>
                 <input type="text" className="main-search"  placeholder='Search...'></input>
+              </div>
+              <div className="right">
+                <Bell className="bell-icon"/>
+                <div className="user">
+                  <img className='profile-picture' height="48px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/2048px-Default_pfp.svg.png" alt="" />
+                  <b className="name">James Hammet</b>
+                  <Arrow className="arrow-icon"/>
+                </div>
               </div>
             </div>
             <div className="body">
