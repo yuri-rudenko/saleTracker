a4cb764539d113fbfb8d1aec6ee9dfe17c23e200
Yurii <yurii.rudenko26@gmail.com>
2025-02-01 21:36:45 +0200

    completed-graphs-css

--- frontend/src/Components/RevenueGraph.jsx
+++ frontend/src/Components/RevenueGraph.jsx
@@ -4,6 +4,26 @@ import React from 'react';
 const RevenueGraph = () => {
     return (
         <div className='revenue-container'>
+
+            <div className="revenue-text">
+
+                <div className="top">
+                    <div className="left">
+                        Revenue
+                    </div>
+                    <div className="right">
+                        <p className="scale">Day</p>
+                        <p className="scale">Week</p>
+                        <p className="scale">Month</p>
+                    </div>
+                </div>
+                <div className="total-revenue">2873₴</div>
+                <div className="increase">
+                    <div className="percent-good">+3.3%</div>
+                    <div className="period">from last period</div>
+                </div>
+
+            </div>
             <LineChart
                 xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
                 series={[
--- frontend/src/Components/ViewsGraph.jsx
+++ frontend/src/Components/ViewsGraph.jsx
@@ -16,11 +16,26 @@ const xLabels = [
 const ViewsGraph = () => {
     return (
         <div className='views-container'>
+            <div className="views-text">
+                <div className="top">
+                    <div className="user-visits">User Visits</div>
+                    <div className="right">
+                        <p className="scale">D</p>
+                        <p className="scale">W</p>
+                        <p className="scale">M</p>
+                    </div>
+                </div>
+                <div className="increase">
+                    <div className="percent-good">+3.3%</div>
+                    <div className="period">from last period</div>
+                </div>
+            </div>
             <BarChart
               height={400}
               series={[
                 { data: uData, label: 'uv', id: 'uvId', color: "#0561FC"},
               ]}
+              slotProps={{ legend: { hidden: true } }}
               xAxis={[{ data: xLabels, scaleType: 'band' }]}
             />
         </div>
