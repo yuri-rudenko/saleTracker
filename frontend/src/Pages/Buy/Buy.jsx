71d41081d5e4ba25ac75442d3796f00f82454c6a
Yurii <yurii.rudenko26@gmail.com>
2025-02-09 01:51:37 +0200

    added-buyingOrders-acardeons

--- frontend/src/Pages/Buy/Buy.jsx
+++ frontend/src/Pages/Buy/Buy.jsx
@@ -1,45 +1,49 @@
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Paper, Table, TableBody, TableCell, TableContainer, TablePagination, TableRow, Typography } from '@mui/material';
 import React from 'react';
+import EnhancedTableHead from '../../Components/EnhancedTableHead';
+import getComparator from '../../functions/getComparator';
+import BuyProductsTable from './BuyProductsTable';
 
-function getDate(dateString) {
-    const [day, month, year] = dateString.split('.');
-    return new Date(year, month - 1, day);
+function createData(date, amount, types, price, status, products) {
+    return {
+        date,
+        amount,
+        types,
+        price,
+        status,
+        products
+    };
 }
 
-function descendingComparator(a, b, orderBy) {
-    if (orderBy === 'date') {
-        if (getDate(b[orderBy]) < getDate(a[orderBy])) {
-            return -1;
-        }
-        if (getDate(b[orderBy]) > getDate(a[orderBy])) {
-            return 1;
-        }
-    } else {
-        if (b[orderBy] < a[orderBy]) {
-            return -1;
-        }
-        if (b[orderBy] > a[orderBy]) {
-            return 1;
-        }
-    }
-    return 0;
-}
+const rows = [
+    createData("03.01.2025", 5, 3, 1700, "Arrived", [{ image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunchi', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }]),
+    createData("03.01.2025", 5, 3, 1700, "Arrived", [{ image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch2', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }]),
+    createData("03.01.2025", 5, 3, 1700, "Arrived", [{ image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch3', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }]),
+    createData("03.01.2025", 5, 3, 1700, "Arrived", [{ image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch4', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }]),
+    createData("03.01.2025", 5, 3, 1700, "Arrived", [{ image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch5', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }]),
+    createData("03.01.2025", 5, 3, 1700, "Arrived", [{ image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch6', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }]),
+    createData("03.01.2025", 5, 3, 1700, "Arrived", [{ image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch7', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }]),
+    createData("03.01.2025", 5, 3, 1700, "Arrived", [{ image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch8', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }]),
+    createData("03.01.2025", 5, 3, 1700, "Arrived", [{ image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch9', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }, { image: 'https://ae-pic-a1.aliexpress-media.com/kf/S14219c43563043fea66f85f455add4d3Y.jpg_960x960q75.jpg_.avif', name: 'Crunch', amount: 3, price: 300, totalPrice: 900, amountInOne: 1 }]),
 
-function getComparator(order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
+];
 
 const headCells = [
     {
-        id: 'amount',
+        id: 'date',
         numeric: false,
+        disablePadding: true,
+        label: 'Date',
+    },
+    {
+        id: 'amount',
+        numeric: true,
         disablePadding: false,
         label: 'Total amount',
     },
     {
         id: 'types',
-        numeric: false,
+        numeric: true,
         disablePadding: true,
         label: 'Product types',
     },
@@ -97,9 +101,129 @@ const headCellsProduct = [
 ];
 
 const Buy = () => {
+
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('calories');
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+    const visibleRows = React.useMemo(
+        () =>
+            [...rows]
+                .sort(getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+        [order, orderBy, page, rowsPerPage],
+    );
+
+
     return (
         <div>
-            buy
+            <div className="buttons">
+                <div className="create-new-product">
+                    <Button variant="outlined">
+                        Create new order
+                    </Button>
+                </div>
+            </div>
+            <Box sx={{ width: '100%' }}>
+                <Paper sx={{ width: '100%', mb: 2 }}>
+                    <TableContainer>
+                        <Table
+                            sx={{ minWidth: 750 }}
+                            aria-labelledby="tableTitle"
+                            size={'medium'}
+                        >
+                            <EnhancedTableHead
+                                order={order}
+                                orderBy={orderBy}
+                                rowCount={rows.length}
+                                headCells={headCells}
+                                setOrder={setOrder}
+                                setOrderBy={setOrderBy}
+                            />
+                            <TableBody>
+                                {visibleRows.map((row, index) => (
+                                    <TableRow>
+                                        <TableCell colSpan={6} style={{ padding: 0 }}>
+                                            <Accordion
+                                                sx={{ boxShadow: 'none', '&:before': { display: 'none' } }}
+                                            >
+                                                <AccordionSummary
+                                                    disableGutters
+                                                    sx={{
+                                                        '.MuiAccordionSummary-content': {
+                                                            margin: '0 !important',
+                                                            minHeight: 'unset',
+                                                            alignItems: 'center',
+                                                        },
+                                                        minHeight: 'unset',
+                                                        padding: 0
+                                                    }}
+                                                >
+                                                    <Table>
+                                                        <TableBody>
+                                                            <TableRow
+                                                                hover
+                                                                tabIndex={-1}
+                                                                key={row.id}
+                                                                sx={{
+                                                                    cursor: 'pointer',
+                                                                    '& td, & th': { borderBottom: 'none' }
+                                                                }}
+                                                            >
+                                                                <TableCell align="left">{row.date}</TableCell>
+                                                                <TableCell align="right">{row.amount}</TableCell>
+                                                                <TableCell align="right">{row.types}</TableCell>
+                                                                <TableCell align="right">{row.price}</TableCell>
+                                                                <TableCell align="right">{row.status}</TableCell>
+                                                            </TableRow>
+                                                        </TableBody>
+                                                    </Table>
+                                                </AccordionSummary>
+                                                <AccordionDetails sx={{ padding: '0 !important' }}>
+                                                    <Typography>
+                                                        <BuyProductsTable rowsData={row.products}/>
+                                                    </Typography>
+                                                </AccordionDetails>
+                                            </Accordion>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                                {emptyRows > 0 && (
+                                    <TableRow
+                                        style={{
+                                            height: 53 * emptyRows,
+                                        }}
+                                    >
+                                        <TableCell colSpan={6} />
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        component="div"
+                        count={rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </Paper>
+            </Box>
         </div>
     );
 }
---
+++ frontend/src/Pages/Buy/BuyProductsTable.jsx
@@ -0,0 +1,140 @@
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel } from '@mui/material';
+import React from 'react';
+
+function createData({ image, name, amount, price, totalPrice, amountInOne }) {
+    return {
+        image,
+        name,
+        amount,
+        price,
+        totalPrice,
+        amountInOne
+    };
+}
+
+const headCells = [
+    {
+        id: 'image',
+        numeric: false,
+        disablePadding: true,
+        label: 'Image',
+    },
+    {
+        id: 'name',
+        numeric: false,
+        disablePadding: false,
+        label: 'Name',
+    },
+    {
+        id: 'amount',
+        numeric: false,
+        disablePadding: true,
+        label: 'Amount',
+    },
+    {
+        id: 'price',
+        numeric: false,
+        disablePadding: true,
+        label: 'Price',
+    },
+    {
+        id: 'totalPrice',
+        numeric: false,
+        disablePadding: true,
+        label: 'Total',
+    },
+    {
+        id: 'amountInOne',
+        numeric: true,
+        disablePadding: true,
+        label: 'Amount in One',
+    },
+];
+
+const BuyProductsTable = (props) => {
+
+    const { rowsData } = props;
+
+    const rows = rowsData.map(row => createData(row));
+
+    return (
+        <div>
+            <TableContainer>
+                <Table
+                    sx={{ minWidth: 750 }}
+                    aria-labelledby="tableTitle"
+                    size={'medium'}
+                >
+                    <TableHead
+                        sx={{
+                            '& .MuiTableCell-root': {
+                                padding: '4px 8px',
+                                fontSize: '0.85rem',   
+                            }
+                        }}
+                    >
+                        <TableRow>
+                            {headCells.map((headCell) => {
+                                if (headCell.id === "image") return (
+                                    <TableCell
+                                        key={headCell.id}
+                                        align={headCell.numeric ? 'right' : 'left'}
+                                        padding={headCell.disablePadding ? 'none' : 'normal'}
+                                    >
+                                        <TableSortLabel>
+                                            {headCell.label}
+                                        </TableSortLabel>
+                                    </TableCell>
+                                )
+                                return (
+                                    <TableCell
+                                        key={headCell.id}
+                                        align={headCell.numeric ? 'right' : 'left'}
+                                        padding={!headCell.disablePadding ? 'none' : 'normal'}
+                                    >
+                                        {headCell.label}
+                                    </TableCell>
+                                )
+                            }
+                            )}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody
+                        sx={{
+                            '& .MuiTableCell-root': {
+                              fontSize: '0.8rem',
+                            }
+                          }}
+                    >
+                        {rows.map((row, index) => {
+
+                            return (
+                                <TableRow
+                                    hover
+                                    tabIndex={-1}
+                                    key={row.id}
+                                    sx={{ cursor: 'pointer', '& td, & th': { borderBottom: '1px solid #f5f5f5' } }}
+                                >
+                                    <TableCell
+                                        component="th"
+                                        scope="row"
+                                        padding="none"
+                                    >
+                                        <img style={{ height: "48px", width: "48px" }} src={row.image} alt="item" />
+                                    </TableCell>
+                                    <TableCell align="left">{row.name}</TableCell>
+                                    <TableCell align="left">{row.amount}</TableCell>
+                                    <TableCell align="left">{row.price}</TableCell>
+                                    <TableCell align="left">{row.totalPrice}</TableCell>
+                                    <TableCell align="right">{row.amountInOne}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
+}
+
+export default BuyProductsTable;
